2e4592760b26b40dbfda64eda6256b43f8ae63ed
Ksenia Shlyonskaya <80854037+x-bananer@users.noreply.github.com>
2021-08-10 22:23:51 +0300

    Delete analysis.js

--- 01/js/analysis.js
+++
@@ -1,8 +0,0 @@
-const path = "/users/download/index.html"
-
-function isHtml(path) {
-    const SEARCH_STRING = ".html";
-    return path.slice(-5) === SEARCH_STRING;
-}
-
-console.log(isHtml(path));
